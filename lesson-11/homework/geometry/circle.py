60523c4e5f69b06de384c34a1f05fb69c7c7c119
Akramjon-682 <akramjonqosimov682@gmail.com>
2025-04-28 12:40:51 +0500

    Create circle.py

---
+++ lesson-11/homework/geometry/circle.py
@@ -0,0 +1,9 @@
+import math
+
+def calculate_area(radius):
+    """Radiusdan doira maydonini hisoblaydi."""
+    return math.pi * radius**2
+
+def calculate_circumference(radius):
+    """Radiusdan doira perimetrini hisoblaydi."""
+    return 2 * math.pi * radius
